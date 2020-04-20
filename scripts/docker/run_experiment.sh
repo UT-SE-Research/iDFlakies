c38aaba3fa6d96c7495d53541077a62ecc353499
winglam <winglam2@illinois.com>
2020-04-20 01:14:30 -0500

    Fixing issue with permissions preventing intermediate results from being saved

--- scripts/docker/run_experiment.sh
+++ scripts/docker/run_experiment.sh
@@ -32,6 +32,12 @@ timeout=$3
 git rev-parse HEAD
 date
 
+modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
+
+mkdir -p /Scratch/all-output/${modifiedslug}_output/
+chown "$SCRIPT_USERNAME" /Scratch/all-output/${modifiedslug}_output/
+chmod 755 /Scratch/all-output/${modifiedslug}_output/
+
 # Update all tooling
 su - "$SCRIPT_USERNAME" -c "cd /home/$SCRIPT_USERNAME/$TOOL_REPO/; git pull"
 
@@ -50,8 +56,10 @@ fi
 su - "$SCRIPT_USERNAME" -c "$script_to_run ${slug} ${rounds} ${timeout}"
 
 # Change permissions of results and copy outside the Docker image (assume outside mounted under /Scratch)
-modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
-mkdir -p "/Scratch/${modifiedslug}_output/misc-output/"
-cp -r "/home/$SCRIPT_USERNAME/output/" "/Scratch/${modifiedslug}_output/misc-output/"
-chown -R $(id -u):$(id -g) /Scratch/${modifiedslug}_output/misc-output/
-chmod -R 777 /Scratch/${modifiedslug}_output/misc-output/
+mkdir -p "/Scratch/all-output/${modifiedslug}_output/misc-output/"
+cp -r "/home/$SCRIPT_USERNAME/output/" "/Scratch/all-output/${modifiedslug}_output/misc-output/"
+chown -R $(id -u):$(id -g) /Scratch/all-output/${modifiedslug}_output/
+chmod -R 777 /Scratch/all-output/${modifiedslug}_output/
+
+chown $(id -u):$(id -g) /Scratch/all-output/
+chmod 777 /Scratch/all-output/
