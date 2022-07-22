8ed860aaa4b284d775e7748391a3ca06c889dcc4
Mahdi <mahdi.khosravi@metu.edu.tr>
2022-07-22 15:35:24 +0300

    detector factory

--- idflakies-core/src/main/java/edu/illinois/cs/dt/tools/detection/detectors/DetectorFactory.java
+++ idflakies-core/src/main/java/edu/illinois/cs/dt/tools/detection/detectors/DetectorFactory.java
@@ -1,9 +1,11 @@
 package edu.illinois.cs.dt.tools.detection.detectors;
 
+import edu.illinois.cs.dt.tools.detection.TestShuffler;
 import edu.illinois.cs.dt.tools.runner.InstrumentingSmartRunner;
 import edu.illinois.cs.testrunner.configuration.Configuration;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DetectorFactory {
@@ -15,6 +17,16 @@ public class DetectorFactory {
         return makeDetector(runner, baseDir, tests, Configuration.config().getProperty("dt.randomize.rounds", 20));
     }
 
+    public static int getClassesSize(List<String> tests) {
+        List<String> classes = new ArrayList<String>();        
+        for (final String test : tests) {
+            final String className = TestShuffler.className(test);
+            if (!classes.contains(className)) {
+                classes.add(className);
+            }
+        }
+        return classes.size();
+    }
     public static Detector makeDetector(final InstrumentingSmartRunner runner, final File baseDir, final List<String> tests, final int rounds) {
         if (detectorType().startsWith("random")) {
             return new RandomDetector(detectorType(), baseDir, runner, rounds, tests);
@@ -24,8 +36,12 @@ public class DetectorFactory {
             return new OriginalDetector(runner, baseDir, rounds, tests);
         } else if (detectorType().equals("smart-shuffle")) {
             return new SmartShuffleDetector(runner, baseDir, rounds, tests, detectorType());
+        }  else if (detectorType().equals("tuscan")){
+            int n = getClassesSize(tests);
+            return new TuscanDetector(runner, baseDir, rounds > n ? n : rounds, detectorType(), tests);
+        } else if (detectorType().equals("alphabetical")) {
+            return new AlphabeticalDetector(runner, baseDir, rounds, detectorType(), tests);
         }
-
         return new RandomDetector("random", baseDir, runner, rounds, tests);
     }
 }
