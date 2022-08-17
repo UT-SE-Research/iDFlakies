9c505cd8b5fe2443b2d0a84f264a399f5f4d5cd2
mmk <mmk@LAPTOP-EA2TFBKV.localdomain>
2022-08-17 10:58:34 +0300

    fixed whitespaced

--- idflakies-core/src/main/java/edu/illinois/cs/dt/tools/utility/Tuscan.java
+++ idflakies-core/src/main/java/edu/illinois/cs/dt/tools/utility/Tuscan.java
@@ -3,13 +3,21 @@ package edu.illinois.cs.dt.tools.utility;
 public class Tuscan {
     public static int[][] generateTuscanPermutations(int arg) {
         if (arg == 1) {
+
             generateOne();
+
         } else if (arg == 3) {
+
             generateThree();
+
         } else if (arg == 5) {
+
             generateFive();
+
         } else {
+
             generateTuscanSquare(arg);
+
         }
         return r;
     }
